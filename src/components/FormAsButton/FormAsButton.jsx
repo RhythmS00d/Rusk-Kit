b24d025298f2cea4f296f8c71af3e11d04291549
RhythmS00d <rhythm.sood69@gmail.com>
2024-01-19 17:39:04 +1100

    added formAsButton component - inherits all form properties

---
+++ src/components/FormAsButton/FormAsButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+export const FormAsButton = ({ label = "Button", style, size = "md", ...props }) => {
+  const buttonSize = {
+    sm: "7em",
+    md: "8em",
+    l: "11em",
+    xl: "13em",
+  };
+  const fontSize = {
+    sm: "1rem",
+    md: "1.5rem",
+    l: "2rem",
+    xl: "2.5rem",
+  };
+  return (
+    <>
+      <form
+        className="button"
+        style={{ width: buttonSize[size], fontSize: fontSize[size], ...style }}
+        {...props}
+      >
+        {label}
+      </form>
+    </>
+  );
+};
